448dcdd8aa004bf00b7b83478b13ea7b4e5d2e4d
Jason Marshall <psion1369@gmail.com>
2011-04-07 11:46:35 -0500

    ver 0.1.2

--- INI-Class.php
+++ INI-Class.php
@@ -1,54 +1,71 @@
 <?php
 
-/*  Copyright 2011 Jason Marshall
-
-    This file is part of INI-class.
-
-    INI-Class is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    INI-Class is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with INI-Class.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+define('INI_AUTOSAVE',1); // 0 is not to save at the close of the class, 1 saves
+	//I really don't know of anywhere else to have an option.  but this is where I would put it.
 
 class parseINI
 {
-	public $ini;			// The INI string, as a full string.
-	public $ini_file;		// The filename.
-	public $ini_hand; 	// The INI file handle.  
-	public $ini_array;	// The INI broken down into an array
+
+	public $ini;		// The INI string, as a full string.  Comments and whitespace stripped.
+	public $ini_file;	// The filename.
+	public $ini_hand; // The INI file handle.  
+	public $ini_array;// The INI broken down into an array
 	
-	
-	/*
-	      I might rewrite the construct function, just
-		   not right now.  When I do, it will be to something that won't have to rewrite
-		   the damn INI file each time.  Next time, I guess. */
-		   
-	function __construct($file)
-	{
-		$this->ini_file=$file;
-if(is_file($file))
+	private $ini_opt; // Options array
+
+function __construct($file)
 {
-	$this->ini_hand=fopen($file,'r');
-	$this->ini=fread($this->ini_hand, filesize($file));
-	fclose($this->ini_hand);
-	}		
-		$this->parseString();
-		$this->ini_hand=fopen($file,'w');
-		}
-		   
-	function parseArray($array = null)
+	$this->iniopt();
+	$this->ini_file=$file;
+	if(!is_file($file)) 
+	{
+		$brad=fopen($file,'w');
+		fwrite($brad,' ');
+		fclose($brad);
+		} else {
+   $inistring=file_get_contents($file);
+   $this->ini=trim(preg_replace('/(\s*[#;].*$)|(^\s*\n)/m', '', $inistring));
+      }
+}
+
+function parseString($string = null)
+{
+	$this->ini_array = array();
+	if($string == null) { $string=$this->ini; }
+	$jones=preg_split('/\n/',$string);
+
+
+
+foreach($jones as $txt)
+{
+	$c=preg_match_all ("/(\\[.*?\\])/is", $txt,$matches);
+  if($c)
+  {
+
+
+      $foo=preg_replace($pattern= "/(\\[)/is", $replacement = '', $txt);
+$section=preg_replace($pattern= "/(\\])/is", $replacement = '', $foo); 
+if(!isset($this->ini_array[$section])) { $this->ini_array[$section] = array(); }
+//echo $section."\n";    
+  } else {
+    $janet=explode($delimiter='=', $txt, $limit = 2); // My Easter Egg.  I love Rocky Horror
+    $colombia=trim($janet[0]);
+    $riff=trim($janet[1]);
+    $this->ini_array[$section][$colombia]=$riff;
+
+  	}
+  	return $this->ini_array;
+}
+
+  
+  
+  }
+  
+  
+function parseArray($array = null)
 	{
 		if ($array == null) { $array=$this->ini_array; }
-      $str="#INI Files Created by Me.\n"; //Just something to set the string file up.
+      $str="#INI Files Created by INI-Class.\n"; //Just something to set the string file up.
 		foreach($array as $section => $nxt_arry)
 		{  $str=$str."[".$section."]\n";
 		   foreach($nxt_arry as $name => $value)
@@ -58,53 +75,29 @@ if(is_file($file))
 			 return($str);
 			}
 			
-	function saveINI($str=null)
-	{
-		if ($str == null ) { $str=$this->ini; }
-		//rewind($this->ini_file);
-		fwrite($this->ini_hand,$str);
-		}
-		
-		
-	function parseString($string = null)
-	{
-		if($string == null) { $string = $this->ini; }
-		$this->ini_array = array();
-		$jones=preg_split($pattern = "/\n/", $string);
-		$re1='(#)';	# Any Single Character 1
-		$re2='(\\[.*?\\])';	# Square Braces 1
-		$re3='.*?';	# Non-greedy match on filler
-		$re4='((?:[a-z][a-z]+))';	# Word 1
+function saveINI($str = null)
+{
+	if($str == null) { $str=$this->ini; }
+	file_put_contents($this->ini_file,$str);
+	}
 
-		foreach($jones as $txt)
-		{
-			 if ((!$c=preg_match_all ("/".$re1."/is", $txt, $matches))OR(!$c=preg_match_all ("/(;)/is", $txt, $matches)))
-			 {
-			 	if ($c=preg_match_all ("/".$re2."/is", $txt, $matches))
-			 	{
-			 		$c=preg_match_all ("/".$re3.$re4."/is", $txt, $matches);			 		
-			 		$section=$matches[1][0];
-			 		$this->ini_array[$section]=array();
-			 		}
-			 	elseif($c=preg_match_all ("/".$re3.$re4."/is", $txt, $matches)) 
-			 	{
-			 		$matches=explode("=",$txt,2);
-			 		$name=$matches[0];
-			 		$value=$matches[1];
-			 		$this->ini_array[$section][$name]=$value;
-			 		}
-			 		}
-			 		}
-			 		}
-			 	
-		
-		   
-	   
-	function __destruct() {
-		$this->saveINI(); //Until I figure out what I really need.
-		fclose($this->ini_hand); }
+function iniopt($opt=null,$value=null)
+{
+	
+	
+	if($opt==null)
+	{
+		$this->ini_opt[1]=1;
+		} else {
+		$this->ini_opt[$opt]=$value;
+		}
+		}
+	
+
+function __destruct()
+{
+	if ($this->ini_opt[1]==1) { $this->saveINI($this->ini); }
+	}
 
 }
-		
-		
-		?>
+   
